9f8d08051258455c8d2d0a0a27b80f7573a71208
Joseph Anane <joseph.anane@ibit-soft.com>
2021-08-28 15:01:36 +0000

    remove dist

--- dist/lib/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomId = void 0;
-const { v4: uuidv4 } = require('uuid');
-const getRandomId = () => {
-    return uuidv4();
-};
-exports.getRandomId = getRandomId;
